ab7ac8f5f53d895375363a99b0b9b73c5844f2da
Arnold Kao <arnoldkao@gmail.com>
2024-10-22 19:55:56 -0400

    Hide annoying overlays.

--- main.lua
+++ main.lua
@@ -7,6 +7,19 @@ SlashCmdList['GREATVAULTSLASHCOMMAND'] = function()
     if frame == nil then
         C_AddOns.LoadAddOn("Blizzard_WeeklyRewards")
         frame = WeeklyRewardsFrame
+        frame:HookScript(
+            "OnShow",
+            function()
+                -- Disable the overlay that reminds the player to go to their
+                -- vault.
+
+                frame.Overlay.Show = function() end
+                frame.Overlay:Hide()
+
+                -- Pass mouse events through the blackout.
+
+                frame.Blackout:EnableMouse(false)
+            end)
     end
 
     if frame:IsVisible() then
