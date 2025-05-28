29431d7d60e712054f46bfccda5149e53c070196
Inkounter <inkounter@gmail.com>
2025-05-28 01:42:35 -0400

    Hook the script even if the addon is already loaded.

--- main.lua
+++ main.lua
@@ -2,30 +2,43 @@ tinsert(UISpecialFrames, 'WeeklyRewardsFrame')
 
 SLASH_GREATVAULTSLASHCOMMAND1 = '/vault'
 
-SlashCmdList['GREATVAULTSLASHCOMMAND'] = function()
-    local frame = WeeklyRewardsFrame
+local frame = WeeklyRewardsFrame
+local hooked = false
+
+local init = function()
+    if hooked then
+        return
+    end
+
     if frame == nil then
         C_AddOns.LoadAddOn("Blizzard_WeeklyRewards")
         frame = WeeklyRewardsFrame
-        frame:HookScript(
-            "OnShow",
-            function()
-                C_Timer.After(0, function()
-                    -- Disable the overlay that reminds the player to go to
-                    -- their vault.
-
-                    if frame.Overlay then
-                        frame.Overlay:Hide()
-                    end
-
-                    -- Pass mouse events through the blackout.
-
-                    if frame.Blackout then
-                        frame.Blackout:EnableMouse(false)
-                    end
-                end)
-            end)
     end
+
+    frame:HookScript(
+        "OnShow",
+        function()
+            C_Timer.After(0, function()
+                -- Disable the overlay that reminds the player to go to
+                -- their vault.
+
+                if frame.Overlay then
+                    frame.Overlay:Hide()
+                end
+
+                -- Pass mouse events through the blackout.
+
+                if frame.Blackout then
+                    frame.Blackout:EnableMouse(false)
+                end
+            end)
+        end)
+
+    hooked = true
+end
+
+SlashCmdList['GREATVAULTSLASHCOMMAND'] = function()
+    init()
 
     if frame:IsVisible() then
         frame:Hide()
